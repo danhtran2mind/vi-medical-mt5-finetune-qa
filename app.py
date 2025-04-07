41a59d06c16c251f71ea25d74df9a17670ec2d83
Danh Tran <cprmmb99@gmail.com>
2025-04-07 16:36:48 +0700

    Update app.py

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ import subprocess
 import os
 import string
 import re
+from underthesea import word_tokenize
 
 def run_shell_command(command):
     process = subprocess.Popen(command, stdout=subprocess.PIPE, shell=True)
@@ -16,7 +17,7 @@ def run_shell_command(command):
 
 def load_model_and_tokenizer(model_path):
     # Load the trained tokenizer
-    tokenizer = AutoTokenizer.from_pretrained(model_path, add_prefix_space=True)
+    tokenizer = AutoTokenizer.from_pretrained(model_path)
 
     # Load the trained model
     model = AutoModelForSeq2SeqLM.from_pretrained(model_path)
@@ -31,37 +32,25 @@ def load_model_and_tokenizer(model_path):
 
     return tokenizer, model, device
 
-def string_processing(text, exclude=None):
-    if exclude is None:
-        exclude = "-"
-    
-    # Define a regular expression pattern to match punctuation marks, excluding the specified ones
-    punctuation_to_handle = ''.join([p for p in string.punctuation if p not in exclude])
-    pattern = r'([{}])'.format(re.escape(punctuation_to_handle))
-    
-    # Use re.sub to replace each punctuation mark with a space before and after it
-    text_with_spaces = re.sub(pattern, r' \1 ', text)
-    
-    # Remove any extra spaces that might have been added
-    text_with_spaces = re.sub(r'\s{2,}', ' ', text_with_spaces).strip()
-    
-    return text_with_spaces
+
 
 def generate_text(tokenizer, model, device, prompt, max_length=100,
                   num_return_sequences=1, top_p=0.95, temperature=0.7):
 
-    prompt = string_processing(prompt)
-    
+    prompt = word_tokenize(prompt, format='text')
     # Tokenize the input prompt
     input_ids = tokenizer.encode(prompt, return_tensors='pt').to(device)
 
     # Generate text
     output = model.generate(
         input_ids,
+        # num_return_sequences=num_return_sequences,
+        no_repeat_ngram_size=2,
+        top_k=50,
         max_length=max_length,
-        num_return_sequences=num_return_sequences,
         top_p=top_p,
         temperature=temperature,
+        do_sample=True
     )
 
     # Convert the generated text back to a string
@@ -72,10 +61,6 @@ def generate_text(tokenizer, model, device, prompt, max_length=100,
 def gradio_generate_text(prompt, max_length=100, num_return_sequences=1, top_p=0.95, temperature=0.7):
     generated_text = generate_text(tokenizer, model, device, prompt, max_length, num_return_sequences, top_p, temperature)
     return generated_text
-
-# def gradio_generate_text(prompt, max_length, num_sequences, top_p, temperature):
-#     # Placeholder for your text generation logic
-#     return f"Generated text based on: {prompt}"
 
 # Ensure the models directory exists
 if not os.path.exists('models'):
@@ -97,12 +82,11 @@ iface = gr.Interface(
     inputs=[
         gr.Textbox(lines=3, label="Input Prompt"),
         gr.Slider(minimum=10, maximum=768, value=32, label="Max Length"),
-        # gr.Slider(minimum=1, maximum=5, value=1, label="Number of Sequences"),
         gr.Slider(minimum=0.1, maximum=1.0, value=0.95, label="Top-p Sampling"),
         gr.Slider(minimum=0.1, maximum=1.0, value=0.7, label="Temperature")
     ],
     outputs=gr.Textbox(lines=5, label="Generated Text"),
-    title="Vietnamese Medical T5 Fine-Tuned Model",
+    title="Vietnamese Medical T5 Fine-Tune Question and Answer",
     description="Generate text using a fine-tuned Vietnamese medical T5 model."
 )
 
